db021a3de3b6fb47f420c37f988ab9638ccf22cf
Daniel Lamblin <dlamblin@gmail.com>
2011-10-31 05:52:41 -0400

    got the cost and then the gradients right finally.

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -18,6 +18,11 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+J = (sum(-y .* log(sigmoid(X * theta)) - (1 - y) .* log(1 - sigmoid(X * theta))) ./ m) + (sum(theta(2:end) .^ 2) * lambda / (2 * m));
+%grad = (sum((sigmoid(X * theta) - y) * ones(1, length(theta)) .* X + (lambda * [0; ones(m-1, 1)] * theta')) ./ m)';
+grad = ((sigmoid(X * theta) - y) * ones(1, length(theta))) .* X;
+grad = (sum(grad) ./ m)';
+grad = grad + (lambda / m * [0; theta(2:end)]);
 
 
 
@@ -25,3 +30,4 @@ grad = zeros(size(theta));
 % =============================================================
 
 end
+
